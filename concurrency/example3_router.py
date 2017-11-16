e28535e1eae1998a5622a96b1d80901120c9000e
Dave Jones <dave@waveform.org.uk>
2017-11-16 15:51:35 +0000

    Simplify router

--- concurrency/example3_router.py
+++ concurrency/example3_router.py
@@ -8,26 +8,20 @@ front.bind('ipc:///tmp/random')
 ready = []
 busy = {}
 
-def handle_front():
-    if ready:
-        client, _, request = front.recv_multipart()
-        worker = ready.pop(0)
-        busy[worker] = client
-        back.send_multipart([worker, _, request])
-
-def handle_back():
-    worker, _, response = back.recv_multipart()
-    if response != b'READY':
-        client = busy.pop(worker)
-        front.send_multipart([client, _, response])
-    ready.append(worker)
-
 poller = zmq.Poller()
 poller.register(back, zmq.POLLIN)
 poller.register(front, zmq.POLLIN)
 while True:
     for sock, flags in poller.poll(1000):
-        if sock == front:
-            handle_front()
-        elif sock == back:
-            handle_back()
+        if sock == back:
+            worker, _, response = back.recv_multipart()
+            if response != b'READY':
+                client = busy.pop(worker)
+                front.send_multipart([client, _, response])
+            ready.append(worker)
+        elif sock == front:
+            if ready:
+                client, _, request = front.recv_multipart()
+                worker = ready.pop(0)
+                busy[worker] = client
+                back.send_multipart([worker, _, request])
